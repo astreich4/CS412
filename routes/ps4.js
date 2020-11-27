f943cecf256f49fe2a8b6e66ecb924e64c14b885
Adam <astreich@bu.edu>
2020-11-27 01:58:48 -0600

    built in cash
    silly commit but implemented a cash with a 15 second timeout

--- routes/ps4.js
+++ routes/ps4.js
@@ -2,9 +2,23 @@ let express = require('express');
 let request = require('request');
 let apicall = require('../config/URLEndpoints');
 
+const redis = require('redis');
+const client = redis.createClient();
+const {promisify} = require('util');
+
+const asyncSet = promisify(client.set).bind(client);
+const asyncGet = promisify(client.get).bind(client);
+const asyncExists = promisify(client.exists).bind(client);
+const asyncExpire = promisify(client.expire).bind(client);
+
 let router = express.Router();
 
 
+client.flushdb((err, success) => {
+    if (err) {
+        throw new Error(err)
+    }
+});
 
 /* GET home page. */
 router.route('/')
@@ -12,13 +26,39 @@ router.route('/')
         res.render('ps4',{page:1})
     })
 
-.post((req,res,next) => {
+.post(async (req,res,next) => {
     //apicall.url = apicall.url + req.body.name;
-    doReq(req)
-        .then(value => {
-            console.log(value)
-            res.render('ps4',{page: 2, val: value })
-        })
+    if ( await asyncExists(req.body.name)){
+        let value = await asyncGet(req.body.name);
+        let response = {
+            key: req.body.name,
+            value: value,
+            cashed: true
+        }
+        console.log(response)
+        res.json(response)
+    } else{
+        doReq(req)
+            .then(async value => {
+                console.log(value);
+                let response = {
+                    key: req.body.name,
+                    value:value,
+                    cashed: false
+                }
+
+                let status = await asyncSet(req.body.name, value);
+                await asyncExpire(req.body.name,15);
+                console.log(status);
+                res.json(response);
+
+            })
+
+
+
+    }
+
+
 
 
 
